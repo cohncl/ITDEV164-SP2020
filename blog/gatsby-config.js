71cf67d05bcf162c5526d6a78407884a728c05d3
charles-cohn <cohncl@gmatc.matc.edu>
2020-01-30 19:51:18 -0600

    Completed Activity 1

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,8 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Blog`,
+    description: `Starting off the semester with a blog`,
+    author: `Charles Cohn`,
+    contact: {
+      name: `Charles Cohn`,
+      company: `Blogs Inc.`,
+      address: `P.O. Box 1234`,
+    }
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
